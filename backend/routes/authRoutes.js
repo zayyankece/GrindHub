55c1cec1ded37357cb949f459e48aba681110b09
nabilrakaiza <nabilraka1234@gmail.com>
2025-06-01 15:09:59 +0700

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { signup, login } = require('../controllers/authController');
 
 // Make sure you have this route:
-// router.post('/signup', signup);
+router.post('/signup', signup);
 router.post('/login', login);
 
 module.exports = router;

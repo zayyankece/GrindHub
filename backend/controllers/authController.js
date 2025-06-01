5b29ca457bdf61d8880abee0253bd0f823361423
nabilrakaiza <nabilraka1234@gmail.com>
2025-06-01 15:28:32 +0700

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -42,7 +42,7 @@ exports.signup = async (req, res) => {
 
     // Insert new user
     const newUser = await pool.query(
-      'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING id, email',
+      'INSERT INTO users (email, password) VALUES ($1, $2)',
       [email, hashedPassword]
     );
 
